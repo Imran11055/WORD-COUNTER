c2af4a8fa67f451442e3cafe6ce4dd6c46303c4f
Imran11055 <108211026+Imran11055@users.noreply.github.com>
2024-04-17 21:58:29 +0500

    Push my word count project

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.bgCyan(`Your sentence word count is ${words.length}`));
